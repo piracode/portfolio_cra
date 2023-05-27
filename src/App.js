677a5a93402d13e73fe6b57e1190362970af8e8f
Martha Villa Martin <34667426+piracode@users.noreply.github.com>
2023-05-27 09:11:52 -0700

    basic intro logo animation with scss

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import './scss/styles.scss'
+import Logo from './components/Logo'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='site-wrapper'>
+      <main className='introduction'>
+        {/* <img src={reactLogo} alt='' /> */}
+        <Logo />
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
