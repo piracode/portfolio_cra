ffc078b1251e26e57e62c3c8d6875c736b99471d
Martha Villa Martin <34667426+piracode@users.noreply.github.com>
2023-05-27 10:46:58 -0700

    tweaked the logo animation and created transition to Home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
 import './scss/styles.scss'
 import Logo from './components/Logo'
+import Home from './pages/Home'
 
 function App() {
+  const [animationComplete, setAnimationComplete] = useState(false)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setAnimationComplete(true)
+    }, 1700)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [])
+
   return (
     <div className='site-wrapper'>
-      <main className='introduction'>
-        {/* <img src={reactLogo} alt='' /> */}
-        <Logo />
-      </main>
+      <BrowserRouter>
+        <main>
+          {!animationComplete ? (
+            <div className='introduction'>
+              <Logo />
+            </div>
+          ) : (
+            <Routes>
+              <Route path='/' element={<Home />} />
+              {/* Other routes here */}
+            </Routes>
+          )}
+        </main>
+      </BrowserRouter>
     </div>
   )
 }
