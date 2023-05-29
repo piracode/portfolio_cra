84cf8f092b69b750162c3b39833510229ae730d2
Martha Villa Martin <34667426+piracode@users.noreply.github.com>
2023-05-29 10:38:47 -0700

    implemented mobile navigation with headlessui-react package + slidein navigation animation and svg icon animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,15 @@
 import Navigation from './Navigation'
+import { ReactComponent as Logo } from '../assets/mvm-logo.svg'
 
 const Header = () => {
-  return <Navigation />
+  return (
+    <header className='header'>
+      {/* <div className='logo-container'> */}
+      <Logo alt='Martha Villa Martin Logo' className='logo-header' />
+      {/* </div> */}
+      <Navigation />
+    </header>
+  )
 }
 
 export default Header
