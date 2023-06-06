2a9067897cffd60412365224737c0ac9fc8d7fd6
Martha Villa Martin <34667426+piracode@users.noreply.github.com>
2023-06-06 09:30:08 -0700

    styled desktop hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -19,16 +19,20 @@ const Hero = () => {
         src={profilePic}
         alt="Martha Villa Martin's profile picture"
       />
-      <div className='hero-intro-box'>
-        <p className='hero-greeting'>{t('greeting')}</p>
-        <h1 className='hero-name'>{t('name')}</h1>
-        <h2 className='hero-role'>{t('role')}</h2>
+      <div className='hero-text-container'>
+        <div className='hero-intro-box'>
+          <p className='hero-greeting'>{t('greeting')}</p>
+          <h1 className='hero-name'>{t('name')}</h1>
+          <h2 className='hero-role'>{t('role')}</h2>
+        </div>
+        <div className='hero-description-box'>
+          <p className='hero-description'> {t('descriptionPart1')}</p>
+          <strong className='hero-subdescription'>
+            {t('descriptionPart2')}
+          </strong>
+        </div>
+        <button className={buttonClassName}>{t('SeeProjectsCTA')}</button>
       </div>
-      <div className='hero-description-box'>
-        <p className='hero-description'> {t('descriptionPart1')}</p>
-        <strong className='hero-subdescription'>{t('descriptionPart2')}</strong>
-      </div>
-      <button className={buttonClassName}>{t('SeeProjectsCTA')}</button>
     </section>
   )
 }
