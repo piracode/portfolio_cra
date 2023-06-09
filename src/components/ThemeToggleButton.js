ed4543a08c728e1a4c6a40c2edb80acb9def4157
Martha Villa Martin <34667426+piracode@users.noreply.github.com>
2023-06-08 21:51:58 -0700

    added some styles to hero and about sections

--- src/components/ThemeToggleButton.js
+++ src/components/ThemeToggleButton.js
@@ -4,9 +4,13 @@ import { useTranslation } from 'react-i18next'
 import { FaSun, FaMoon } from 'react-icons/fa'
 
 const ThemeToggleButton = ({ className, isMobile }) => {
+  // Accessing the theme context using useContext hook
   const { isDarkMode, toggleTheme } = useContext(ThemeContext)
+
+  // Accessing translation function using useTranslation hook
   const { t } = useTranslation()
 
+  // Event handler for toggling the theme
   const handleToggle = () => {
     toggleTheme()
   }
@@ -14,24 +18,18 @@ const ThemeToggleButton = ({ className, isMobile }) => {
   return (
     <div className={`nav-button-container ${className}`} onClick={handleToggle}>
       <div className='nav-button secondary-button'>
+        {/* Conditional rendering of icon based on the current theme */}
         {isDarkMode ? (
           <span className='navigation-link-icon'>
-            <FaSun
-              className={`theme-icon ${
-                isDarkMode ? 'dark-icon' : 'light-icon'
-              }`}
-            />
+            <FaSun />
           </span>
         ) : (
           <span className='navigation-link-icon'>
-            <FaMoon
-              className={`theme-icon ${
-                isDarkMode ? 'dark-icon' : 'light-icon'
-              }`}
-            />
+            <FaMoon />
           </span>
         )}
       </div>
+      {/* Displaying translated text based on the current theme */}
       <span className='navigation-link-text'>
         {t(`toggleTheme.${isDarkMode ? 'dark' : 'light'}`)}
       </span>
--- src/pages/About.js
+++ src/pages/About.js
@@ -26,65 +26,69 @@ const About = () => {
 
   return (
     <section id='about' className='about'>
-      <h3 className='section-title'>
-        <span className='parallax-link-number'> 01. </span>
-        <span className='title-text'>{t('titleSectionAbout')}</span>
-        <span className='title-line'>&nbsp;</span>
-      </h3>
-      <div className='about-text-container'>
-        <p className='about-paragraph-1'>{t('paragraph1')}</p>
-        <p className='about-paragraph-2'>{t('paragraph2')}</p>
-        <p className='about-paragraph-3'>{t('paragraph3')}</p>
-        <p className='about-paragraph-4'>{t('paragraph4')}</p>
-        <a
-          className='about-youtubeLink'
-          href='https://www.youtube.com/watch?v=aMr2pchOeBA'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          {t('youtubeLink')}
-        </a>
-        <a
-          className='about-facebookLink'
-          href='https://www.facebook.com/quillerina.paperart'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          {t('QuillerinaLink')}
-        </a>
-        <p className='about-paragraph-4-continued'>
-          {t('paragraph4-continued')}
-        </p>
-      </div>
-      <button className='primary-button about-button'>
-        <a className='button-link' href='#'>
-          {t('seePhotosCTA')}
-        </a>
-      </button>
+      <article className='about-text-container'>
+        <h3 className='section-title'>
+          <span className='parallax-link-number'> 01. </span>
+          <span className='title-text'>{t('titleSectionAbout')}</span>
+          <span className='title-line'>&nbsp;</span>
+        </h3>
+        <div className='about-text-container'>
+          <p className='about-paragraph-1'>{t('paragraph1')}</p>
+          <p className='about-paragraph-2'>{t('paragraph2')}</p>
+          <p className='about-paragraph-3'>{t('paragraph3')}</p>
+          <p className='about-paragraph-4'>{t('paragraph4')}</p>
+          <a
+            className='about-youtubeLink'
+            href='https://www.youtube.com/watch?v=aMr2pchOeBA'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            {t('youtubeLink')}
+          </a>
+          <a
+            className='about-facebookLink'
+            href='https://www.facebook.com/quillerina.paperart'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            {t('QuillerinaLink')}
+          </a>
+          <p className='about-paragraph-4-continued'>
+            {t('paragraph4-continued')}
+          </p>
+        </div>
+        <button className='primary-button about-button'>
+          <a className='button-link' href='#'>
+            {t('seePhotosCTA')}
+          </a>
+        </button>
+      </article>
 
-      <h4 className='about-stack-title'>{t('techStack')}</h4>
-      <div className='about-stack-icons-container'>
-        <SiHtml5 title='HTML5' className='about-stack-icon' />
-        <SiCss3 title='CSS3' className='about-stack-icon' />
-        <FaGit title='Git' className='about-stack-icon' />
-        <SiJavascript title='JavaScript' className='about-stack-icon' />
-        <SiGithub title='GitHub' className='about-stack-icon' />
-        <SiGit title='Git' className='about-stack-icon' />
-        <SiPhp title='PHP' className='about-stack-icon' />
-        <FaSass title='Sass' className='about-stack-icon' />
-        <SiTailwindcss title='Tailwind CSS' className='about-stack-icon' />
-        <SiWordpress title='WordPress' className='about-stack-icon' />
-        <SiShopify title='Shopify' className='about-stack-icon' />
-        <SiFigma title='Figma' className='about-stack-icon' />
-        <SiAdobephotoshop
-          title='Adobe Photoshop'
-          className='about-stack-icon'
-        />
-        <SiAdobexd title='Adobe XD' className='about-stack-icon' />
-        <SiReact title='React' className='about-stack-icon' />
-        <SiCanva title='Canva' className='about-stack-icon' />
-        {/* <SiScrumalliance title='Scrum Alliance' className='about-stack-icon' /> */}
-      </div>
+      <article className='tech-stack-container'>
+        <h4 className='about-stack-title'>{t('techStack')}</h4>
+        <div className='about-stack-icons-container'>
+          <SiHtml5 title='HTML5' className='about-stack-icon' />
+          <SiCss3 title='CSS3' className='about-stack-icon' />
+          <FaGit title='Git' className='about-stack-icon' />
+          <SiJavascript title='JavaScript' className='about-stack-icon' />
+          <SiGithub title='GitHub' className='about-stack-icon' />
+          <SiGit title='Git' className='about-stack-icon' />
+          <SiPhp title='PHP' className='about-stack-icon' />
+          <FaSass title='Sass' className='about-stack-icon' />
+          <SiTailwindcss title='Tailwind CSS' className='about-stack-icon' />
+          <SiWordpress title='WordPress' className='about-stack-icon' />
+          <SiShopify title='Shopify' className='about-stack-icon' />
+          <SiFigma title='Figma' className='about-stack-icon' />
+          <SiAdobephotoshop
+            title='Adobe Photoshop'
+            className='about-stack-icon'
+          />
+          <SiAdobexd title='Adobe XD' className='about-stack-icon' />
+          <SiReact title='React' className='about-stack-icon' />
+          <SiCanva title='Canva' className='about-stack-icon' />
+          {/* <SiScrumalliance title='Scrum Alliance' className='about-stack-icon' /> */}
+        </div>
+      </article>
     </section>
   )
 }
